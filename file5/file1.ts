b05280808535146aa4f5ae40a3115d655403e3a9
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-07-18 01:43:31 +0530

    any and unknown type

---
+++ file5/file1.ts
@@ -0,0 +1,15 @@
+// Any Type 
+
+// The Any type is the most flexible type in typescript . It essentially turns off type checking for the variables or expressions it is applied to .
+
+let favNumber : any = 5; // now the error is gone typechecking is disable 
+
+favNumber = "Hola"
+
+
+// use Cases 
+
+// working with dynamic data dealing . when dealing with data from dynamic sources like user inputs , network responses or deserialized JSON objects , the any type can be useful 
+
+// migration from js : when migrating an existing js codebase to ts , using the any type can be a convenient way to quickly annotate variables and functions without immediately specifying their precise types
+
---
+++ file5/file2.ts
@@ -0,0 +1,26 @@
+// unknown types
+ // the unknown type is a safer alternative to any because it still enforces type checking and type safety 
+// variables of type unknown can hold values of any type , but you must perform typeChecks or type assertions before using them in specific ways 
+
+// let myfavNum  = 55 ;
+// myfavNum = true // this is type checking 
+
+// myfavNum.map(() => {
+  
+// }) // property map doesn't exists on type number 
+
+let num2 : unknown ;
+
+num2 = 5 ;
+num2 = "Subh" ;
+num2 = true ;
+
+if(typeof num2 === "number"){
+  console.log(num2 + 5)
+}else if (typeof num2 === "string"){
+  console.log("hello " + num2)
+}else if(typeof num2 === "boolean"){
+  console.log(num2);
+}
+
+//unknown useCases are generics
