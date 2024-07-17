ba077afa045091545a9e7411975e86e7c0babb93
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-07-18 01:19:56 +0530

    boolean and bigInt types

---
+++ file4/file1.ts
@@ -0,0 +1,15 @@
+// write a typescript function called isEven that takes a number as a parameter and returns true if the number is even and false otherwise 
+
+function isEven (a: number ):boolean{
+  return a % 2 === 0;
+}
+
+console.log(isEven(5));
+
+// write a typescript function called isDivisible by 4 and 8 that takes a number as a parameter and returns true if the number is divisible and false otherwise 
+
+function isDivisible(a: number): boolean{
+  return a % 4 == 0 || a % 8 === 0;
+}
+
+console.log(isDivisible(16));
---
+++ file4/file2.ts
@@ -0,0 +1,14 @@
+// bigInt 
+
+// It is a built-in type that allows you to work with numbers that are larger than the range supported by the regular number type.
+
+// BigInt literals are written by appending the n suffix to an integer literal 
+// In js we cant read the whole number beyond 2 raise to 53
+
+// const maxNumber = Number.MAX_SAFE_INTEGER;
+// let maxNumber : bigint = 9007199254740992n
+// console.log(maxNumber)
+
+//end way 
+let anotherBigNumber = BigInt("90071992547409925")
+console.log(anotherBigNumber);
